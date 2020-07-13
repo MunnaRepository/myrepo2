4e7bb6a7930c49ec6884ee0c3fe84033ac7a796b
User <User@DESKTOP-1B8VO4K>
2020-07-13 18:24:24 +0530

    Article Service Spring Boot app.

---
+++ ArticleService/src/test/java/com/kloudone/ArticleService/ArticleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kloudone.ArticleService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
