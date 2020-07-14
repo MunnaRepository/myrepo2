76d897cb96ba76279c425830f04d61c880a1047d
User <User@DESKTOP-1B8VO4K>
2020-07-14 09:04:00 +0530

    Enabling Discovery client

--- ArticleService/src/main/java/com/kloudone/ArticleService/ArticleServiceApplication.java
+++ ArticleService/src/main/java/com/kloudone/ArticleService/ArticleServiceApplication.java
@@ -2,7 +2,9 @@ package com.kloudone.ArticleService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ArticleServiceApplication {
 
